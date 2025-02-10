0f9f775045e4af9ba499cdd915131767ecd734e4
David Natan <rdpalominop@ucvvirtual.edu.pe>
2025-02-10 02:16:40 +0100

    new deploy henrycommerce drents

--- front/src/helpers/DeployArticle.helpers.ts
+++ front/src/helpers/DeployArticle.helpers.ts
@@ -3,6 +3,14 @@ import { IDeployArticleProps } from "@/interfaces/types";
 export const deployArticleHelpers: IDeployArticleProps[] = [
     {
         id: 1, 
+        title: 'DTV',
+        text: 'Ingenius is an innovative platform powered by artificial intelligence that allows users to create courses quickly and efficiently. Through personalized prompts and the integration of their own resources, creators can generate structured, high-quality educational content. The AI optimizes course organization, tailoring it to the needs of the target audience and facilitating the production of training materials without requiring advanced technical knowledge.',
+        tech: 'TypeScript - React - Next - Express - PostgreSQL - Tailwind - Figma - Photoshop',
+        img: '/LogoDTV.png',
+        link: 'https://dtv-mu.vercel.app'
+    },
+    {
+        id: 2, 
         title: 'Ingenius',
         text: 'Ingenius is an innovative platform powered by artificial intelligence that allows users to create courses quickly and efficiently. Through personalized prompts and the integration of their own resources, creators can generate structured, high-quality educational content. The AI optimizes course organization, tailoring it to the needs of the target audience and facilitating the production of training materials without requiring advanced technical knowledge.',
         tech: 'NextJS - React - Tailwind - Typescript - Figma - Photoshop',
@@ -10,7 +18,7 @@ export const deployArticleHelpers: IDeployArticleProps[] = [
         link: 'https://ingenius-lydg.vercel.app'
     },
     {
-        id: 2, 
+        id: 3, 
         title: 'AgroDexports',
         text: 'Ingenius is an innovative platform powered by artificial intelligence that allows users to create courses quickly and efficiently. Through personalized prompts and the integration of their own resources, creators can generate structured, high-quality educational content. The AI optimizes course organization, tailoring it to the needs of the target audience and facilitating the production of training materials without requiring advanced technical knowledge.',
         tech: 'TypeScript - HTML - CSS - PrismaORM - PostgresSQL - NextJS - React - NextAuth - SoketIO - Figma - Photoshop',
@@ -18,36 +26,28 @@ export const deployArticleHelpers: IDeployArticleProps[] = [
         link: 'https://ingenius-lydg.vercel.app'
     },
     {
-        id: 3, 
+        id: 4, 
+        title: 'Henry Commerce',
+        text: 'Ingenius is an innovative platform powered by artificial intelligence that allows users to create courses quickly and efficiently. Through personalized prompts and the integration of their own resources, creators can generate structured, high-quality educational content. The AI optimizes course organization, tailoring it to the needs of the target audience and facilitating the production of training materials without requiring advanced technical knowledge.',
+        tech: 'TypeScript - JavaScript - CSS - NextJS - React - Postgress - Express - Tailwind - Figma - Phothosop',
+        img: '/HenryCommerce.png',
+        link: 'https://ingenius-lydg.vercel.app'
+    },
+    {
+        id: 5, 
         title: 'Drents',
         text: 'Ingenius is an innovative platform powered by artificial intelligence that allows users to create courses quickly and efficiently. Through personalized prompts and the integration of their own resources, creators can generate structured, high-quality educational content. The AI optimizes course organization, tailoring it to the needs of the target audience and facilitating the production of training materials without requiring advanced technical knowledge.',
         tech: 'JavaScript - TypeScript - CSS - HTML - React - Vite - Redux - Postgres - Express - Figma - Photoshop',
         img: '/LogoDrents.png',
-        link: 'https://ingenius-lydg.vercel.app'
+        link: 'https://drents.vercel.app'
     },
     {
-        id: 4, 
+        id: 6, 
         title: 'Start4',
         text: 'Ingenius is an innovative platform powered by artificial intelligence that allows users to create courses quickly and efficiently. Through personalized prompts and the integration of their own resources, creators can generate structured, high-quality educational content. The AI optimizes course organization, tailoring it to the needs of the target audience and facilitating the production of training materials without requiring advanced technical knowledge.',
         tech: 'HTML - CSS - JavaScript - PHP - PHPMyAdmin - Figma - Photoshop',
         img: '/LogoStart4.png',
         link: 'https://www.manager-start4.nl'
-    },
-    {
-        id: 5, 
-        title: 'Inport Commerce',
-        text: 'Ingenius is an innovative platform powered by artificial intelligence that allows users to create courses quickly and efficiently. Through personalized prompts and the integration of their own resources, creators can generate structured, high-quality educational content. The AI optimizes course organization, tailoring it to the needs of the target audience and facilitating the production of training materials without requiring advanced technical knowledge.',
-        tech: 'TypeScript - JavaScript - CSS - NextJS - React - Postgress - Express - Tailwind - Figma - Phothosop',
-        img: '/AgroDexportsLo',
-        link: 'https://ingenius-lydg.vercel.app'
-    },
-    {
-        id: 6, 
-        title: 'DTV',
-        text: 'Ingenius is an innovative platform powered by artificial intelligence that allows users to create courses quickly and efficiently. Through personalized prompts and the integration of their own resources, creators can generate structured, high-quality educational content. The AI optimizes course organization, tailoring it to the needs of the target audience and facilitating the production of training materials without requiring advanced technical knowledge.',
-        tech: 'TypeScript - React - Next - Express - PostgreSQL - Tailwind - Figma - Photoshop',
-        img: '/LogoDTV.png',
-        link: 'https://dtv-mu.vercel.app'
     },
     {
         id: 7, 
