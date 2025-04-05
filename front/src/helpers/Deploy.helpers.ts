e1f5376d7a5f0f25170e86f96a91e83d202a1352
David Natan <rdpalominop@ucvvirtual.edu.pe>
2025-04-05 23:11:40 +0200

    new deploy

--- front/src/helpers/Deploy.helpers.ts
+++ front/src/helpers/Deploy.helpers.ts
@@ -3,20 +3,20 @@ import { IDeployContentProps } from "@/interfaces/types"
 export const deployContentHelpers: IDeployContentProps[] = [
     {
         id: 1, 
+        image: '/LogoLookai.png',
+        link: 'https://lookai-tawny.vercel.app',
+        imageSize: 'w-[10rem]'
+    },
+    {
+        id: 2, 
         image: '/LogoIngeniusOpacity.svg',
         link: 'https://ingenius-lydg.vercel.app',
         imageSize: 'w-[4rem]'
     },
     {
-        id: 2, 
+        id: 3, 
         image: '/START4.svg',
         link: 'https://www.manager-start4.nl',
         imageSize: 'w-[6rem]'
     },
-    {
-        id: 3, 
-        image: '/COINVELOPER.svg',
-        link: 'https://www.coinveloper.com',
-        imageSize: 'w-[6rem]'
-    }
 ]
